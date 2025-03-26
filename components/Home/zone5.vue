9381e0fe6f7bcf165f25e84bafc432b566ce55ca
jajerrr <jaajer@gmail.com>
2025-03-26 18:46:56 +0700

    ..addzone6 not finish panorama slider

--- components/Home/zone5.vue
+++ components/Home/zone5.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="w-full h-screen relative overflow-hidden">
        
-  <div class="absolute top-[20vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
+  <div class="absolute top-[15vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
     <div class="Rubik text-[30px] text-white">
       SHOW CASE
     </div>
-    <HomeZone6/>
+    
   </div>
+  <!-- <div class ="absolute top-[40vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
+    <HomeZone6/>
+  </div> -->
+  
 
 
       <div>
